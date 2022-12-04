e088ac1a939368b1779a2b4cc6ef1ced1b70b4ac
Taiye Ajimati <taiyeajimati@gmail.com>
2022-12-04 09:24:43 +0400

    First office commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ module.exports = {
           DEFAULT: '1.5rem',
           sm: '3rem',
           lg: '6rem',
-          '2xl': '9rem',
+          '2xl': '4rem',
+          // '3xl': '4rem',
         },
 
         center: true,
@@ -34,7 +35,9 @@ module.exports = {
         bold: 700,
         extrabold: 800,
       },
-
+      screens: {
+        '3xl': '1700px',
+      },
     },
   },
   plugins: [],
