b7648607ee740c3d14cdd31f6d24a6e4843ec98b
Taiye <taiyeajimati@gmail.com>
2022-12-05 22:46:05 +0400

    Last home commit

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -9,26 +9,28 @@ function Card({ country, dispOption, myregion }) {
         <div className='card wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10  justify-items-center'>
             {/* <div className='card wrapper grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-10  justify-items-center'> */}
             {/* <div className='card wrapper flex flex-col lg:flex-row lg:flex-wrap gap-x-7 gap-y-10'> */}
-            {
-                mapKey.map(({ name, region, flags, population, capital }, index) => (
-                    <div key={name.common} className='Card  lg:w-[250px]'>
-                        <Link className='cursor-pointer list-none' to={`details/${name.common}`}>
-                            <div className="card bg-white dark:bg-DarkmdEl dark:text-white shadow-lg  hover:shadow-xl hover:scale-105 duration-300 ease-in-out rounded-lg w-[100%]  md:min-w-[250px]">
-                                <div className="flag min-h-[200px] lg:h-[150px]  overflow-hidden">
-                                    <img className='rounded-t-lg  w-[100%]  object-contain' src={flags.png} alt="" />
-                                </div>
-                                <div className="desc py-6 lg:py-0 lg:pb-8 flex flex-col items-start  ml-6 ">
-                                    <h1 className='text-xl font-bold mb-4'>{name.common}</h1>
-                                    <div className="population flex "><p className='font-[700] text-md '>Population:</p><p className='text-md  ml-2'>{population}</p></div>
-                                    <div className="region flex"><p className='font-[700] text-md inline'>Region:</p><p className='text-md inline ml-2'>{region}</p></div>
-                                    <div className="capital flex"> <p className='font-[700] text-md'>Capital:</p><p className='text-md inline ml-2'>{capital}</p></div>
-                                </div>
+
+            {mapKey.map(({ name, region, flags, population, capital }, index) => (
+                <div key={name.common} className='Card  lg:w-[250px] shadow-lg  hover:shadow-xl hover:scale-105 duration-300 ease-in-out '>
+                    <Link className='cursor-pointer list-none' to={`details/${name.common}`}>
+                        <div className="card bg-white dark:bg-DarkmdEl dark:text-white rounded-lg w-[100%]  md:min-w-[250px]">
+                            <div className="flag min-h-[200px] lg:h-[150px]  overflow-hidden">
+                                <img className='rounded-t-lg  w-[100%]  object-contain' src={flags.png} alt="" />
                             </div>
-                        </Link>
-                    </div>
-                ))
+                            <div className="desc py-6 lg:py-0 lg:pb-8 flex flex-col items-start  ml-6 ">
+                                <h1 className='text-xl font-bold mb-4'>{name.common}</h1>
+                                <div className="population flex "><p className='font-[700] text-md '>Population:</p><p className='text-md  ml-2'>{population}</p></div>
+                                <div className="region flex"><p className='font-[700] text-md inline'>Region:</p><p className='text-md inline ml-2'>{region}</p></div>
+                                <div className="capital flex"> <p className='font-[700] text-md'>Capital:</p><p className='text-md inline ml-2'>{capital}</p></div>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            ))
+
             }
         </div>
+
     )
 }
 
--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { ArrowLongLeftIcon } from '@heroicons/react/24/outline';
@@ -10,19 +10,30 @@ import { useKeyGen } from 'react-key-from-object'
 
 
 
-
 function Details({ country, dispOption, myregion }) {
+
+    const [borderurl, setBorderurl] = useState('https://restcountries.com/v3.1/alpha/IND')
+
+    // useEffect(() => {
+    //     axios.get(borderurl)
+    //       .then(response => {
+    //         setCountry(response.data)
+    //         console.log(country)
+    //       })
+    //   }, [name])
 
     const keyGen = useKeyGen();//Asign auto generated keys to mapped items
 
     const { name } = useParams()
     const FilterReg = country.filter((cont) => cont.name.common === name);
-    //const border = country.filter((cont) => cont.cca3 === "IND");
+    const border = country.filter((cont) => cont.cca3 === "IND");
 
+    // const native = border.name
+    // console.log(Object.keys(border))
 
     // {
     //     border.map((bor) =>
-    //         console.log(bor)
+    //         console.log(bor.name.nativeName.eng)
     //     )
     // }
     // console.log(FilterReg.region)
@@ -37,12 +48,6 @@ function Details({ country, dispOption, myregion }) {
     // )
 
 
-
-
-    // console.log(name)
-
-
-
     const mapKey = dispOption ? country : FilterReg;
     // const theCountry = country.name = name
     // console.log(theCountry)
@@ -53,7 +58,7 @@ function Details({ country, dispOption, myregion }) {
     return (
         <div className="details-container min-h-[100vh] mt-10 lg:mt-16 dark:text-white">
             <div className='container'>
-                <button onClick={() => backHome(-1)} className='flex items-center gap-x-1 shadow-lg text-sm text-LtModTxtmdInp bg-white dark:bg-DarkmdEl dark:text-white px-4 py-1 lg:px-9 lg:py-4 dark:hover:opacity-50   hover:bg-gray-300 hover:text-black  duration-300 ease-in-out'><ArrowLongLeftIcon className='w-5 text-black dark:text-white' /> Back</button>
+                <button onClick={() => backHome(-1)} className='flex items-center gap-x-1 shadow-lg text-sm text-LtModTxtmdInp bg-white dark:bg-DarkmdEl dark:text-white px-4 py-1 md:px-9 md:py-4 dark:hover:opacity-50   hover:bg-gray-300 hover:text-black  duration-300 ease-in-out'><ArrowLongLeftIcon className='w-5 text-black dark:text-white' /> Back</button>
                 {FilterReg.map(({ region, subregion, population, capital, flags, currencies, borders }) => (
                     <div key={keyGen.getKey(region)} className="about-country lg:flex items-center gap-x-5 lg:gap-x-10 ">
                         <img className='my-14 lg:h-[300px] ' src={flags.svg} alt="" />
@@ -61,7 +66,8 @@ function Details({ country, dispOption, myregion }) {
                             <div className="topLevel md:gap-x-12 lg:flex">
                                 <div className="desc1 flex-1 flex flex-col  items-start text-left space-y-1 mb-6">
                                     <h1 className='text-xl font-bold mb-2'>{name}</h1>
-                                    <div className="nativeName flex "><p className='font-[700] text-md '>Native Name:</p><p className='text-md  ml-2'>{name}</p></div>
+
+                                    <div className="nativeName flex "><p className='font-[700] text-md '>Native Name:</p><p className='text-md  ml-2'>{name.nativeName}</p></div>
                                     <div className="population flex "><p className='font-[700] text-md '>Population:</p><p className='text-md  ml-2'>{population}</p></div>
                                     <div className="region flex"><p className='font-[700] text-md inline'>Region:</p><p className='text-md inline ml-2'>{region}</p></div>
                                     <div className="sub-region flex"><p className='font-[700] text-md inline'>Sub Region:</p><p className='text-md inline ml-2'>{subregion}</p></div>
@@ -78,8 +84,8 @@ function Details({ country, dispOption, myregion }) {
                             <div className="desc3 flex flex-col items-start">
                                 <h1 className='text-md font-bold mb-1'>Border Countries</h1>
                                 <ul className='flex gap-x-2'>
-                                    {borders.map((border, index) => (
-                                        <li key={index}><a className='px-3 py-1 shadow-md dark:bg-DarkmdEl capitalize' href={`/details/${border.name}`}> {border}</a></li>
+                                    {borders && borders.map((border, index) => (
+                                        <li key={index}><a className='px-3 py-1 shadow-md dark:bg-DarkmdEl capitalize' href={borderurl}> {border}</a></li>
                                     ))}
                                 </ul>
                             </div>
